251249b386044d5532d324a42cca69ca2be70e9b
DHRUV10122004 <dhruvkhanna021@gmail.com>
2025-07-13 17:33:19 +0530

    added file for attaching tokens for sending requests to protected routes

---
+++ frontend/src/api.jsx
@@ -0,0 +1,18 @@
+// src/api.js
+import axios from "axios";
+
+const api = axios.create(); // no baseURL
+
+// Interceptor to attach token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
